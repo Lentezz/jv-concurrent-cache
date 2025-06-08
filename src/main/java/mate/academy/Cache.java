23262858fca3ba316903856f2a5dd10e4fb6f9aa
Lentezz <yaroslav.golovan@gmail.com>
2025-06-08 21:53:29 +0300

    Implement cache logic

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -1,23 +1,63 @@
 package mate.academy;
 
+import java.util.HashMap;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Cache<K, V> {
+    private final HashMap<K, V> cache;
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+
+    public Cache() {
+        cache = new HashMap<>();
+    }
+
     public V get(K key) {
-        return null;
+        lock.readLock().lock();
+        V val;
+        try {
+            val = cache.get(key);
+        } finally {
+            lock.readLock().unlock();
+        }
+        return val;
     }
 
     public void put(K key, V value) {
-
+        lock.writeLock().lock();
+        try {
+            cache.put(key, value);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public void remove(K key) {
-
+        lock.writeLock().lock();
+        try {
+            cache.remove(key);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public void clear() {
-
+        lock.writeLock().lock();
+        try {
+            cache.clear();
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public int size() {
-        return -1;
+        lock.readLock().lock();
+        int size;
+        try {
+            size = cache.size();
+        } finally {
+            lock.readLock().unlock();
+        }
+        return size;
     }
 }
